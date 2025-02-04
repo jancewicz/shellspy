bd9b78087e8f7a6a3f38241e7e8f94218449a022
jancewicz <jancewicz.pawel1@gmail.com>
2025-02-04 19:52:11 +0100

    created infinite loop that ask user for commands and prints their output unitl user enters "exit"

--- cmd/shellspy/main.go
+++ cmd/shellspy/main.go
@@ -9,16 +9,29 @@ import (
 
 // Your CLI goes here!
 func main() {
-	command := shellspy.ReadUserInput()
 
-	cmd, err := shellspy.CommandFromInput(command)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		command := shellspy.ReadUserInput()
+		if command == "exit" {
+			break
+		}
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		cmd, err := shellspy.CommandFromInput(command)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		file, err := os.Create("logger.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
